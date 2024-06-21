aedf295efc1110fe830bbd141756829d82e6a560
AmanPriyanshu <aman@robustintelligence.com>
2024-06-21 15:35:16 -0700

    update to v 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='generator-prompt-kit',
-    version='1.0.0',
+    version='1.0.2',
     packages=find_packages(),
     description='A Python Library for Automated Generator Prompting and Dataset Generation',
     long_description=open('README.md').read(),
